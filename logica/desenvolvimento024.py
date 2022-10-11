0309c0d9696676c6ab6ce18a5c70b75705151e17
Walter Silva <108630688+waltersilva23@users.noreply.github.com>
2022-10-11 11:41:43 -0300

    Create desenvolvimento024.py

---
+++ logica/desenvolvimento024.py
@@ -0,0 +1,8 @@
+nome = str(input('\nDigite seu nome completo: ')).strip()
+print('\nAnalisando seu nome...')
+sem_espaço = len(nome) - nome.count(' ')
+print(f'\nSeu nome tem ao todo {sem_espaço} letras')
+primeiro = nome.split()
+print(f'\nSeu primeiro nome tem {len(primeiro[0])} letras')
+letra = str(input('\nQual letra você deseja saber quantas vezes aparece em seu nome? '))
+print(f'\nA letra {letra.upper()} foi escolhida e aparece {nome.count(letra)}')
