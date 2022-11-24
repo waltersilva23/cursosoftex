ea70cc8102dfe3649950e5950304474ad0d350e1
Walter Silva <108630688+waltersilva23@users.noreply.github.com>
2022-11-24 11:42:00 -0300

    primeiro projeto react

---
+++ react/atividade1.1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Criei Meu Primeiro Projeto React</h1>
+        <p>Para não perder o costume</p>
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
