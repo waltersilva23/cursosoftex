6ff3391aabe0d5241bc207230f32d3df2594b664
Walter Silva <walthier23@gmail.com>
2022-12-06 21:58:29 -0300

    criando atividade 2.2

---
+++ react/atividade2.2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Body from './components/Body';
+import './App.css'
+
+function App() {
+  return (
+
+      <>
+        <Header/>
+        <Body/>
+      </>
+    
+  );
+}
+
+export default App;
---
+++ react/atividade2.2/src/components/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Body () {
+    return (
+        <body>
+            <h2>Aprendendo componentes</h2>
+        </body>
+    );
+};
+
+export default Body;
