ffc135998eb755ae760af256af81273f3c019256
Walter Silva <walthier23@gmail.com>
2023-01-20 21:03:56 -0300

    Update index.js

--- web_service/atividade3.2/index.js
+++ web_service/atividade3.2/index.js
@@ -6,11 +6,44 @@ servidor.use(express.json());
 
 const alunos = ['Alice', 'Alan', 'Pedro'];
 
+//Retorna um aluno
 servidor.get('/alunos/:index', (req,res) => {
     const { index } = req.params;
 
     return res.json(alunos[index]);
-})
+});
+
+//Retorna todos os alunos
+servidor.get('/alunos', (req,res) => {
+    return res.json(alunos)
+});
+
+//Adiciona um novo aluno
+servidor.post('/cursos', (req,res) => {
+    const { nome } = req.body;
+    alunos.push(nome);
+
+    return res.json(alunos);
+});
+
+//Atualizar a lista de alunos
+servidor.put('/cursos/:index', (req,res) => {
+    const { index } = req.params;
+    const { nome } = req.body;
+
+    alunos[index] = nome;
+    return res.json(alunos);
+});
+
+//Excluir aluno da lista
+servidor.delete('/cursos/:index', (req,res) => {
+    const { index } = req.params;
+
+    alunos.splice(index, 1);
+    return res.json({ message: "O aluno foi excluido da lista"});
+});
+
+
 
 
 
